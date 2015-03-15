97d58b34889ec5afbda2590b228e951f68288060
rerb <rerb@mbp.local>
2015-03-14 21:24:59 -0700

    Redirect from root to story list view.

--- aashe_bulletin/urls.py
+++ aashe_bulletin/urls.py
@@ -1,6 +1,7 @@
 import aashe.aasheauth.urls
 from django.conf.urls import patterns, include, url
 from django.contrib import admin
+from django.views.generic import RedirectView
 
 from views import BulletinHelpView
 
@@ -9,13 +10,21 @@ admin.autodiscover()
 urlpatterns = patterns(
     '',
 
-    url(r'^newsletter/', include('bulletin.urls', namespace='bulletin')),
+    url(r'^$',
+        RedirectView.as_view(pattern_name='plugins:story-list'),
+        name='front-page'),
 
-    url(r'^accounts/', include(aashe.aasheauth.urls)),
+    url(r'^newsletter/',
+        include('bulletin.urls',
+                namespace='bulletin')),
+
+    url(r'^accounts/',
+        include(aashe.aasheauth.urls)),
 
     url(r'^help/',
         BulletinHelpView.as_view(),
         name='help'),
 
-    url(r'^admin/', include(admin.site.urls)),
+    url(r'^admin/',
+        include(admin.site.urls)),
 )
