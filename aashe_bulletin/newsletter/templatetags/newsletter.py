cbef987b803d6163fb8418d73525ae7d8da9d880
rerb <bob.erb@aashe.org>
2015-10-08 12:34:04 -0400

    Make unfeatured_posts_categories return Categories
    Not sub-categories.

--- aashe_bulletin/newsletter/templatetags/newsletter.py
+++ aashe_bulletin/newsletter/templatetags/newsletter.py
@@ -4,17 +4,25 @@ from django import template
 register = template.Library()
 
 
+class PostCategoryIsNotSubcategory(Exception):
+    pass
+
+
 @register.assignment_tag
 def unfeatured_posts_categories(posts):
     """Return a list of Categories, in alphabetical order, representing
     the Categories attached to the unfeatured posts in `posts`.
+
+    All Categories assigned to posts should be subcategories (i.e., their
+    parent attribute should point to another Category object).
     """
-    category_dict = {}
+    category_set = set()
     for post in posts:
         if post.feature:
             continue
-        category_dict[post.category.name] = post.category
-    category_list = []
-    for category in sorted(category_dict):
-        category_list.append(category)
-    return category_list
+        try:
+            category_name = post.category.parent.name
+        except AttributeError:
+            raise PostCategoryIsNotSubcategory(post.category)
+        category_set.add(category_name)
+    return sorted(list(category_set))
