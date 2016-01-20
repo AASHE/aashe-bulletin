7113c674756745101398b3b3e7a0edd10c7eb103
rerb <bob.erb@aashe.org>
2016-01-20 14:08:10 -0500

    Update settings.SCREEN_IMAGE_LICENSE_TEXT

--- aashe_bulletin/settings.py
+++ aashe_bulletin/settings.py
@@ -161,16 +161,16 @@ AASHE_DRUPAL_COOKIE_DOMAIN = os.environ['AASHE_DRUPAL_COOKIE_DOMAIN']
 
 SCREEN_IMAGE_UPLOADS = True
 SCREEN_IMAGE_LICENSE_TEXT = """
-By clicking "Yes," you are granting to AASHE an irrevocable,
-royalty-free, non-exclusive and perpetual license to use the submitted
-image in the AASHE Bulletin e-newsletter and website, and you are hereby
-representing and warranting that you own all the rights to the submitted
-image, or have obtained all necessary licenses and/or permissions to use
-the submitted image, and that AASHE's use of such image in the AASHE
-Bulletin e-newsletter and website will not infringe the rights of any
-third party, including but not limited to intellectual property rights,
-or any other rights protected by law (such as the right to privacy or
-right of publicity).
+By checking this box, you are granting AASHE an irrevocable,
+royalty-free, nonexclusive, and perpetual license to use the submitted
+file(s) in publications, newsletters, resources or promotional
+materials, and you are hereby representing and warranting that you own
+all the rights to the submitted file(s), or have obtained all
+necessary licenses and/or permissions to use the submitted file(s),
+and that AASHE's use of such file(s) will not infringe the rights of
+any third party, including but not limited to intellectual property
+rights, or any other rights protected by law (such as the right to
+privacy or right of publicity).
 """
 
 # MAX_STORY_TITLE_LENGTH = 90
