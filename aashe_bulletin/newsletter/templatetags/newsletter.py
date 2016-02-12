22bfb0bb82073ac6394d1a5117a57443cd661a10
rerb <bob.erb@aashe.org>
2016-02-12 16:52:04 -0500

    Patch template tag to handle >1 Category per Post
    I.e., template tag newsletter.unfeatured_posts_categories.

--- aashe_bulletin/newsletter/templatetags/newsletter.py
+++ aashe_bulletin/newsletter/templatetags/newsletter.py
@@ -11,7 +11,8 @@ class PostCategoryIsNotSubcategory(Exception):
 @register.assignment_tag
 def unfeatured_posts_categories(posts):
     """Return a list of Categories, in alphabetical order, representing
-    the Categories attached to the unfeatured posts in `posts`.
+    the Categories attached to the unfeatured posts in `posts` as
+    primary categories.
 
     All Categories assigned to posts should be subcategories (i.e., their
     parent attribute should point to another Category object).
@@ -21,8 +22,8 @@ def unfeatured_posts_categories(posts):
         if post.feature:
             continue
         try:
-            category_name = post.category.parent.name
+            category_name = post.primary_category.parent.name
         except AttributeError:
-            raise PostCategoryIsNotSubcategory(post.category)
+            raise PostCategoryIsNotSubcategory(post.primary_category)
         category_set.add(category_name)
     return sorted(list(category_set))
