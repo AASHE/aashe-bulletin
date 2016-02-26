79f3df5456a00b2e973adb6cd93af166c8e0670b
rerb <bob.erb@aashe.org>
2016-02-26 11:22:14 -0500

    Add django.contrib.flatpages to INSTALLED_APPS
    Avoids warning about unsupported feature in Django v1.9.

--- aashe_bulletin/settings.py
+++ aashe_bulletin/settings.py
@@ -41,8 +41,9 @@ INSTALLED_APPS = (
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
-    'django.contrib.sessions',
+    'django.contrib.flatpages',
     'django.contrib.messages',
+    'django.contrib.sessions',
     'django.contrib.sites',
     'django.contrib.staticfiles',
 
