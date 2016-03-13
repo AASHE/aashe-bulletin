ba6fa1c47ec851571f667721a34d00dc2fb031b2
rerb <bob.erb@aashe.org>
2016-03-12 23:55:46 -0500

    Make fortune update on click

--- aashe_bulletin/settings.py
+++ aashe_bulletin/settings.py
@@ -210,6 +210,7 @@ if es.username:
 
 HAYSTACK_SIGNAL_PROCESSOR = 'haystack.signals.RealtimeSignalProcessor'
 
+
 def is_member(user):
     try:
         aasheuser = user.aasheuser
@@ -223,7 +224,19 @@ SEARCH_USER_FAILS_TEST_URL = '/search-permission-denied/'
 
 REST_FRAMEWORK = {
     'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
-    'PAGE_SIZE': 10
+    'PAGE_SIZE': 10,
+    # Use Django's standard `django.contrib.auth` permissions,
+    # or allow read-only access for unauthenticated users.
+    'DEFAULT_PERMISSION_CLASSES': [
+        'rest_framework.permissions.DjangoModelPermissionsOrAnonReadOnly'],
+    'DEFAULT_THROTTLE_CLASSES': (
+        'rest_framework.throttling.AnonRateThrottle',
+        'rest_framework.throttling.UserRateThrottle'
+    ),
+    'DEFAULT_THROTTLE_RATES': {
+        'anon': '100/minute',
+        'user': '100/minute'
+    }
 }
 
 AWS_ACCESS_KEY_ID = os.environ.get("AWS_ACCESS_KEY_ID", None)
--- aashe_bulletin/urls.py
+++ aashe_bulletin/urls.py
@@ -51,6 +51,10 @@ urlpatterns = patterns(
         LatestNewsFeedView(),
         name='latest-news-category-feed'),
 
+    url(r'^fortune/',
+        include('fortune.urls',
+                namespace='fortune')),
+
     url(r'^',
         include('bulletin.urls',
                 namespace='bulletin')),
